45e78443457c2a965d6003cf2198327eaef9c81b
Bibhuranjan <141688141+bibhuranjan23@users.noreply.github.com>
2024-01-24 21:47:19 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,13 @@
+var n1 =6;
+var n2 =4;
+
+console.log(n1+"+"+n2+"="+(n1+n2));
+
+//Template Literal
+console.log(`${n1}+${n2}=${n1+n2}`);
+
+console.log(`the value of n1 is ${n1}`);
+console.log(n1+"-"+n2+"="+(n1-n2));
+console.log(n1+"*"+n2+"="+(n1*n2));
+console.log(n1+"/"+n2+"="+(n1/n2));
+console.log(n1+"%"+n2+"="+(n1%n2));
